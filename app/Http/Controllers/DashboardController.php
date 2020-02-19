0d859325dba3583caba7e8901a4a69513e3750ca
orbachinujbuk <orbachinujbuk@gmail.com>
2020-02-20 01:13:02 +0600

    SAD3

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -84,26 +84,26 @@ class DashboardController extends Controller
             }
 
             $totalpresentarray = [];
-            for ($i=0; $i < 15; $i++) {
-                $totalpresentarray[$i]['count'] = 0;
-                $totalpresentarray[$i]['date'] = Carbon::now()->subDays($i)->format('Y-m-d');
-                foreach ($queryTeachers as $teacher) {
-                    $attendanceforarray = Attendance::where(DB::raw("DATE_FORMAT(timestampdata, '%Y-%m-%d')"), Carbon::now()->subDays($i)->format('Y-m-d'))
-                                                    ->where('device_id', $teacher->institute->device_id)
-                                                    ->where('device_pin', $teacher->device_pin)
-                                                    ->first();
-                    if (!empty($attendanceforarray)) {
-                        $totalpresentarray[$i]['count']++;
-                    }
-                }
-                if($totalpresentarray[$i]['count'] == 0) {
-                    unset($totalpresentarray[$i]);
-                }
-            }
-            $totalpresentarray = array_values($totalpresentarray);
-            if(count($totalpresentarray) > 7) {
-                $totalpresentarray = array_slice($totalpresentarray, 0, 7);
-            }
+            // for ($i=0; $i < 15; $i++) {
+            //     $totalpresentarray[$i]['count'] = 0;
+            //     $totalpresentarray[$i]['date'] = Carbon::now()->subDays($i)->format('Y-m-d');
+            //     foreach ($queryTeachers as $teacher) {
+            //         $attendanceforarray = Attendance::where(DB::raw("DATE_FORMAT(timestampdata, '%Y-%m-%d')"), Carbon::now()->subDays($i)->format('Y-m-d'))
+            //                                         ->where('device_id', $teacher->institute->device_id)
+            //                                         ->where('device_pin', $teacher->device_pin)
+            //                                         ->first();
+            //         if (!empty($attendanceforarray)) {
+            //             $totalpresentarray[$i]['count']++;
+            //         }
+            //     }
+            //     if($totalpresentarray[$i]['count'] == 0) {
+            //         unset($totalpresentarray[$i]);
+            //     }
+            // }
+            // $totalpresentarray = array_values($totalpresentarray);
+            // if(count($totalpresentarray) > 7) {
+            //     $totalpresentarray = array_slice($totalpresentarray, 0, 7);
+            // }
             // dd(count($totalpresentarray));
             $totalpresentarray = array_reverse($totalpresentarray);
 
