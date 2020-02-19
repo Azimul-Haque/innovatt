8238ae52282d87f2a69ee77f31c92f477f9f361e
orbachinujbuk <orbachinujbuk@gmail.com>
2020-02-19 20:21:04 +0600

    SAD3

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -37,16 +37,16 @@ class DashboardController extends Controller
                return redirect()->route('dashboard.upazillas.ateo', Auth::user()->unique_key);
             } else {
                 $queryTeachers = User::where('role', 'headmaster')
-                    ->orWhere('role', 'teacher')
-                    ->where('upazilla_id', Auth::user()->upazilla_id)
-                    ->get();
+                                     ->orWhere('role', 'teacher')
+                                     ->orWhere('role', 'officeassistant')
+                                     ->where('upazilla_id', Auth::user()->upazilla_id)
+                                     ->get();
             }
-
+            
             $totalpresenttoday = 0;
             $totallateentrytoday = 0;
             $totalearlyleavetoday = 0;
             foreach ($queryTeachers as $teacher) {
-
                 $attendance = Attendance::where(DB::raw("DATE_FORMAT(timestampdata, '%Y-%m-%d')"), "=", Carbon::now()->format('Y-m-d'))
                     ->where('device_id', $teacher->institute->device_id)
                     ->where('device_pin', $teacher->device_pin)
