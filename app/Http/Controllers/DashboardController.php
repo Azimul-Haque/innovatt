32cd603bfd8f796ed885a24b3fefd2004ac94753
orbachinujbuk <orbachinujbuk@gmail.com>
2020-03-04 23:58:51 +0600

    SAd33

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -59,13 +59,10 @@ class DashboardController extends Controller
                     $totalpresenttoday++;
                 }
 
-                // late er ekhane kaaj ache
-                // late er ekhane kaaj ache
-                // late er ekhane kaaj ache
                 $late = Attendance::where(DB::raw("DATE_FORMAT(timestampdata, '%Y-%m-%d')"), "=", Carbon::now()->format('Y-m-d'))
                                   ->where('device_id', $teacher->institute->device_id)
                                   ->where('device_pin', $teacher->device_pin)
-                                  ->where(DB::raw("DATE_FORMAT(timestampdata, '%h:%i')"), ">", date('h:i', strtotime('09:00')))
+                                  ->where(DB::raw("DATE_FORMAT(timestampdata, '%h:%i')"), ">", date('h:i', strtotime($teacher->institute->entrance)))
                                   ->first();
                 if (!empty($late)) {
                     $totallateentrytoday++;
