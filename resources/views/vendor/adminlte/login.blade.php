7fca32d2b8370af8501b929a52e011c21421bc2e
orbachinujbuk <orbachinujbuk@gmail.com>
2020-03-28 02:32:33 +0600

    ASD3

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -11,7 +11,7 @@
 @section('body')
     <div class="login-box">
         <div class="login-logo">
-            <img src="{{ asset('images/logo.png') }}" style="max-width: 70px; height: auto;">
+            <img src="{{ asset('images/logo.png') }}" style="max-width: 70px; height: auto;"><br/>
             <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}">{!! config('adminlte.logo', '<b>Admin</b>LTE') !!}</a>
         </div>
         <!-- /.login-logo -->
